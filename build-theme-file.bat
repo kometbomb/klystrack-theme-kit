a9b8ce39e0a2da2d91e1ff19d5d3aa8412a14116
Tero Lindeman <kometbomb@kometbomb.net>
2015-03-20 17:52:23 +0200

    Template kit Windows (batch) version

---
+++ build-theme-file.bat
@@ -0,0 +1,20 @@
+@echo off
+echo Building your theme...
+mkdir temp
+echo Building fonts...
+bin\makebundle.exe temp\8x8.fnt Template\font
+bin\makebundle.exe temp\7x6.fnt Template\font7x6
+bin\makebundle.exe temp\tiny.fnt Template\tiny
+echo Copying colors...
+copy /y Template\colors.txt temp\colors.txt
+echo Copying graphics...
+copy /y Template\bevel.* temp
+copy /y Template\vu.* temp
+copy /y Template\analyzor.* temp
+copy /y Template\logo.* temp
+echo Bundling everything together...
+bin\makebundle.exe MyNewTheme temp
+echo ---
+echo Your new theme is now packed in the file called "MyNewTheme"!
+echo Copy the file in klystrack\res and restart klystrack.
+pause
